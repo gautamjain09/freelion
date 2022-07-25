09ba50ffffbd5e9de3983544cc92a4ff74a0397d
gautamjain09 <gautamjain0918@gmail.com>
2022-07-25 21:33:22 +0530

    search_footer

--- lib/Screens/search_screen.dart
+++ lib/Screens/search_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:freelion/Support_Widgets/web/search_footer.dart';
 import 'package:freelion/Support_Widgets/web/search_header.dart';
 import 'package:freelion/Support_Widgets/web/search_tabs.dart';
+import 'package:freelion/colors.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({Key? key}) : super(key: key);
@@ -12,17 +14,57 @@ class SearchScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            SearchHeader(),
-            Padding(
+          children: [
+            // < ------------------- Search Header ------------------------------>
+
+            const SearchHeader(),
+
+            // < ------------------- Search Tabs ------------------------------>
+
+            const Padding(
               padding: EdgeInsets.only(left: 150),
-              child: SearchTabs(),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: SearchTabs(),
+              ),
             ),
-            Divider(
+            const Divider(
               height: 0,
               thickness: 1,
             ),
-            // Search results
+
+            // <------------------ Search results --------------------------->
+
+            // < ------------------- Paginationc ------------------------------>
+
+            SizedBox(
+              width: double.infinity,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "< Previous",
+                      style: TextStyle(fontSize: 16, color: blueColor),
+                    ),
+                  ),
+                  const SizedBox(width: 20),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Next >",
+                      style: TextStyle(fontSize: 16, color: blueColor),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            // <---------------------- Search Footer -------------------------->
+
+            const SearchFooter(),
           ],
         ),
       ),
